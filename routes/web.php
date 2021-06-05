a1b74f8dcf7f86ce7a88f4684886257f1a417aa9
mohsinhassan <mastermind_mohsin@hotmail.com>
2021-06-05 15:25:55 +0800

    laravel app with vuejs and authentication added

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// Route::get('{any}', function () {
+//     return view('app');
+// })->where('any', '.*');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('books', 'App\Http\Controllers\BookController@index');
+// Route::group(['prefix' => 'book'], function () {
+//     Route::post('add', 'BookController@add');
+//     Route::get('edit/{id}', 'BookController@edit');
+//     Route::post('update/{id}', 'BookController@update');
+//     Route::delete('delete/{id}', 'BookController@delete');
+// });
+
+/*Route::prefix('book')->group(function () {
+    Route::get('{any}', function () {
+        return view('app');
+    })->where('any', '.*');
+});
+*/
+
+
+
+//Route::get('/book', [App\Http\Controllers\BookController::class, 'index'])->name('book');
+// Route::get('/home', function () {
+//          return view('app');
+//     })->name('home');
+
+
+// Route::any('/', function () {
+//     return view('app');
+// });
